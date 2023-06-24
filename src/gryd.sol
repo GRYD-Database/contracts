533891c78b01f3af6838a1c61f8dda924f22e299
Haseeb Raja <has33braja@gmail.com>
2023-06-25 04:18:33 +0500

    chore: adding queue contract for nodes testing

--- src/gryd.sol
+++ src/gryd.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "./queue.sol";
 
 contract GRYD is Initializable, ERC20Upgradeable, UUPSUpgradeable {
+    using Queue for Queue.NodesQueue;
     address public owner;
+    Queue.NodesQueue nodesQueue;
 
     modifier onlyOwner() {
         require(owner == _msgSender(), "Not authorized");
@@ -26,6 +29,12 @@ contract GRYD is Initializable, ERC20Upgradeable, UUPSUpgradeable {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function buyStorage(address buyer){}
+
+    function addNodes(address node) public {
+        nodesQueue.
     }
 
     function _authorizeUpgrade(
